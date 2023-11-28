8894d3600386670fed7b0ffb7e3f6d1186f05063
sammo3182 <sammo3182@sina.com>
2023-11-28 10:28:45 +0800

    one more sentence about political interests, fixed #9

---
+++ R/surveyInfo.R
@@ -0,0 +1,24 @@
+# info table ---------------------------------------------------------------------
+
+library(tidyverse)
+library(rio)
+library(here)
+
+dcpo_input_raw |>
+  select(year, survey) |>
+  distinct() |>
+  arrange(survey, year) |>
+  mutate(creator = NA,
+         version = NA,
+         link = NA) |>
+  export(here("data-raw", "survey_info_macrointerest.xlsx"))
+
+
+
+# reference ----------------------------------------------------------------------
+
+tb_ref <- import(here("data-raw", "surveys_data.csv")) |> 
+  select(survey, year, archive, surv_program, file_id, data_link) |> 
+  distinct()
+
+export(tb_ref, here("data-raw", "survey_info_full.xlsx"))
